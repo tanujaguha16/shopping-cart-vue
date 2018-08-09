8f8ef772de66abfa09774714a6bb60e2e330acc7
tanuja <tanuja@capitalnumbers.com>
2018-08-09 18:15:08 +0530

    add files and folders

---
+++ src/App.vue
@@ -0,0 +1,84 @@
+<template>
+  <div id="app">
+  <div class="stepwizard">
+    <div class="stepwizard-row setup-panel">
+        <div class="stepwizard-step">
+            <a href="#/box-selector" type="button" class="btn btn-primary btn-circle">1</a>
+            <p>Step 1</p>
+        </div>
+        <div class="stepwizard-step">
+            <a href="#/item-selector" type="button" class="btn btn-default btn-circle" disabled="disabled">2</a>
+            <p>Step 2</p>
+        </div>
+        <div class="stepwizard-step">
+            <a href="#step-3" type="button" class="btn btn-default btn-circle" disabled="disabled">3</a>
+            <p>Step 3</p>
+        </div>
+    </div>
+</div>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+.stepwizard-step p {
+    margin-top: 10px;
+}
+
+.stepwizard-row {
+    display: table-row;
+}
+
+.stepwizard {
+    display: table;
+    width: 100%;
+    position: relative;
+}
+
+.stepwizard-step button[disabled] {
+    opacity: 1 !important;
+    filter: alpha(opacity=100) !important;
+}
+
+.stepwizard-row:before {
+    top: 14px;
+    bottom: 0;
+    position: absolute;
+    content: " ";
+    width: 100%;
+    height: 1px;
+    background-color: #ccc;
+    z-order: 0;
+
+}
+
+.stepwizard-step {
+    display: table-cell;
+    text-align: center;
+    position: relative;
+}
+
+.btn-circle {
+  width: 30px;
+  height: 30px;
+  text-align: center;
+  padding: 6px 0;
+  font-size: 12px;
+  line-height: 1.428571429;
+  border-radius: 15px;
+}
+</style>
---
+++ src/components/BoxSelector.vue
@@ -0,0 +1,78 @@
+<template>
+   <b-container class="bv-example-row">
+   {{changed_text}} <br/> <br/>
+      <b-row>
+        <b-col>
+            <label for="white_box">
+               <div class="outer_box">
+                  <div class="meddle_box">
+                    White Box
+                  </div>
+                </div>
+            </label>
+            <input type="radio" class="radio"  id="white_box" value="white_box" v-model = "box" v-on:change="updateDate('White Box')">
+        </b-col>
+        <b-col>
+           <label for="black_box">
+              <div class="outer_box">
+                <div class="meddle_box">
+                  Black Box
+                </div>
+              </div>
+            </label>
+            <input type="radio" class="radio"  id="black_box" value="black_box" v-model = "box" v-on:change="updateDate('Black Box')">
+        </b-col>
+    </b-row>
+</b-container>
+</template>
+<script>
+export default {
+  name: 'BoxSelector',
+  data () {
+    return {
+      msg: 'Select a box',
+      subtext: 'You are the first to select a box',
+      box: 'white_box',
+      changed_text: 'By default white box selected'
+    }
+  },
+  methods: {
+    updateDate (datas) {
+      this.changed_text = 'You selected ' + datas
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+.radio{
+  display: none;
+}
+.outer_box{
+  width: 400px;
+  height: 400px;
+  background-color: grey;
+  padding-top: 100px;
+}
+.meddle_box{
+  width: 200px;
+  height: 200px;
+  background-color: white;
+  margin-left: 100px;
+}
+</style>
